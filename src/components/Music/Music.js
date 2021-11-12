0cd75ecf7e82b4fe2968dc48a0cc96d1b1385b5b
olegweremyew <olegweremey1994@mail.ru>
2021-11-12 23:06:26 +0300

    NavLink

---
+++ src/components/Music/Music.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import c from "./Music.module.css";
+
+const Music = (props) => {
+    return (
+        <div>
+            50 cent cool man
+        </div>
+    );
+}
+
+export default Music;

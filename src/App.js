1083bfc8a22769c2828210564e72b0d395333fdc
OlegWeremyew <olegweremey1994@mail.ru>
2021-11-12 08:54:17 +0300

    image logo

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-    Hello
-    </div>
-  );
+    return (
+        <main>
+          <header>
+            <img src="https://pbs.twimg.com/media/EUmpUI6UEAImg9a.jpg" />
+          </header>
+        </main>
+    );
 }
 
 export default App;

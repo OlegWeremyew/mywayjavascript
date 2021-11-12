0f02aecd8f0dc072648978ec62f779e46e98aea0
olegweremyew <olegweremey1994@mail.ru>
2021-11-12 23:33:25 +0300

    Dialogs

--- src/components/Dialogs/Dialogs.js
+++ src/components/Dialogs/Dialogs.js
@@ -3,8 +3,32 @@ import c from "./Dialogs.module.css";
 
 const Dialogs = (props) => {
     return (
-        <div>
-            Dialogs
+        <div className={c.dialogs}>
+            <div className={c.dialogItems}>
+                <div className={c.dialog + " " +c.active}>
+                    Oleg
+                </div>
+                <div className={c.dialog}>
+                    Homa
+                </div>
+                <div className={c.dialog}>
+                    Diana
+                </div>
+                <div className={c.dialog}>
+                    Fat cat
+                </div>
+                <div className={c.dialog}>
+                    Alena
+                </div>
+                <div className={c.dialog}>
+                    Igor
+                </div>
+            </div>
+            <div className={c.messages}>
+                <div className="{c.message}">Hi</div>
+                <div className="{c.message}">Ohohoh</div>
+                <div className="{c.message}">beer</div>
+            </div>
         </div>
     );
 }

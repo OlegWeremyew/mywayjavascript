3fcc23a3d63cbe91dfa886b8ce0ff407dd2b2fdf
OlegWeremyew <olegweremey1994@mail.ru>
2021-11-18 12:32:49 +0300

    function map for MyPost and Dialogs

--- src/components/Dialogs/Dialogs.js
+++ src/components/Dialogs/Dialogs.js
@@ -27,7 +27,7 @@ let dialogsData = [
     {id: 6, name: "Igor"},
 ];
 
-let messageData = [
+let messagesData = [
     {id: 1, message: "Hi"},
     {id: 2, message: "Hello? how are you"},
     {id: 3, message: "You"},
@@ -36,24 +36,17 @@ let messageData = [
     {id: 6, message: "Melon is yellow"},
 ];
 
+let dialogsElements = dialogsData.map(d => <DialogItem name={d.name} id={d.id}/>)
+let messagesElements = messagesData.map(m => <Message message={m.message} id={m.id}/>)
+
 const Dialogs = (props) => {
     return (
         <div className={c.dialogs}>
             <div className={c.dialogItems}>
-                <DialogItem name={dialogsData[0].name} id={dialogsData[0].id}/>
-                <DialogItem name={dialogsData[1].name} id={dialogsData[1].id}/>
-                <DialogItem name={dialogsData[2].name} id={dialogsData[2].id}/>
-                <DialogItem name={dialogsData[3].name} id={dialogsData[3].id}/>
-                <DialogItem name={dialogsData[4].name} id={dialogsData[4].id}/>
-                <DialogItem name={dialogsData[5].name} id={dialogsData[5].id}/>
+                {dialogsElements}
             </div>
             <div className={c.messages}>
-                <Message message={messageData[0].message} id={messageData[0].id} />
-                <Message message={messageData[1].message} id={messageData[1].id} />
-                <Message message={messageData[2].message} id={messageData[2].id} />
-                <Message message={messageData[3].message} id={messageData[3].id} />
-                <Message message={messageData[4].message} id={messageData[4].id} />
-                <Message message={messageData[5].message} id={messageData[5].id} />
+                {messagesElements}
             </div>
         </div>
     );
--- src/components/Profile/MyPosts/MyPosts.js
+++ src/components/Profile/MyPosts/MyPosts.js
@@ -2,32 +2,31 @@ import React from 'react';
 import c from './MyPosts.module.css'
 import Posts from "./Post/Posts";
 
-let postData = [
+let posts = [
     {id: 1, message: "Hi, how are you?'", likesCount: 12},
     {id: 2, message: "I'm ok, and you?", likesCount: 7},
+    {id: 3, message: "Potato", likesCount: 7},
+    {id: 4, message: "Big pig", likesCount: 7},
 ];
+
+let postsElents = posts.map(p => <Posts message={p.message} likesCount={p.likesCount} id={p.id}/>)
 
 const MyPosts = () => {
     return (
-            <div className={c.postsBlock}>
-                <h3>My posts</h3>
+        <div className={c.postsBlock}>
+            <h3>My posts</h3>
+            <div>
                 <div>
-                    <div>
-                        <textarea></textarea>
-                    </div>
-                    <div>
-                        <button>Add post</button>
-                    </div>
+                    <textarea></textarea>
                 </div>
-                <div className={c.posts}>
-                    <Posts message={postData[0].message}
-                           likesCount={postData[0].likesCount}
-                           id={postData[0].id}/>
-                    <Posts message={postData[1].message}
-                           likesCount={postData[1].likesCount}
-                           id={postData[1].id}/>
+                <div>
+                    <button>Add post</button>
                 </div>
             </div>
+            <div className={c.posts}>
+                {postsElents}
+            </div>
+        </div>
     );
 }
 

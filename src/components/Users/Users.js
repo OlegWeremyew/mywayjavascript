64b335bade81f9c3c6cab1b583d7ea774f07c892
olegweremyew <olegweremey1994@mail.ru>
2022-01-04 22:31:17 +0300

    class 52 (Side Effect)

--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -4,16 +4,19 @@ import * as axios from "axios";
 import userPhoto from "../../assets/images/user.png"
 
 const Users = (props) => {
-    if (props.users.length === 0) {
 
-        axios.get('https://social-network.samuraijs.com/api/1.0/users')
-            .then(response => {
-                props.setUsers(response.data.items)
-            })
+    let getUsers = () => {
+        if (props.users.length === 0) {
+            axios.get('https://social-network.samuraijs.com/api/1.0/users')
+                .then(response => {
+                    props.setUsers(response.data.items)
+                })
+        }
     }
 
     return (
         <div>
+            <button onClick={getUsers}>get users</button>
             {
                 props.users.map(m => <div key={m.id}>
                     <span>
